c545b2bb889d0c6779a457670ebfd2c47b88c1b9
Nicole Goris <nicolegoris@hotmail.nl>
2023-12-01 13:06:25 +0100

    #4 Details zichtbaar

--- views/DetailDoc.php
+++ views/DetailDoc.php
@@ -2,22 +2,12 @@
 require_once "../views/ItemDoc.php";
 
 class DetailDoc extends ItemDoc {
-    //Onderstaande komt uit de class ItemDoc
-    //private function getItemFromDb() {}
-    //protected function showId() {}            --> niet weergeven
-    //protected function showName() {}
-    //protected function showImg() {}           --> groot plaatje
-    //protected function showPrice() {} 
-    //protected function showDescription() {}
-    //protected function showCartButton() {}    --> alleen als je bent ingelogd
-
-    //protected function showContent() { 
-    //    $this->showName();
-    //    $this->showImg();         --> groot
-    //    $this->showPrice(); 
-    //    $this->showDescription();
-    //    $this->showCartButton();  --> alleen ingelogd
-    //}
+    protected function showContent()
+    {
+        $this->showItem("details", array(
+            array('id' => '4', 'filename' => 'Fabelfruit.png', 'name' => 'Fabelfruit', 'price' => '24.95', 'description' => 'Een leuk spel')
+        ));
+    }
 }
 
 ?> 
