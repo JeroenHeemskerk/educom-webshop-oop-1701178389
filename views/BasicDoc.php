b5c06791a2b2fe405aa75445b7f893a9b0ce8cbc
Nicole Goris <nicolegoris@hotmail.nl>
2023-12-01 12:12:54 +0100

    #4 ItemDoc omgeschreven voor shop top5 details

--- views/BasicDoc.php
+++ views/BasicDoc.php
@@ -1,24 +1,44 @@
 <?php
 require_once "HtmlDoc.php";
 
-class BasicDoc extends HtmlDoc {
+class BasicDoc extends HtmlDoc 
+{
     protected $data;
-    public function __construct($myData) {
+    public function __construct($myData) 
+    {
         $this->data = $myData;
-      }
+    }
     private function showTitle() {echo '<title>' . $this->data['page'] . '</title>';}
     private function showCssLinks() {echo' <link rel="stylesheet" href="../CSS/stylesheet.css">';}
     private function showHeader() {echo '<header><h1>Hier moet de page komen vanuit data</h1></header>';}
     private function showMenu() {echo 'Ik weet nog niet hoe het menu moet.<br>';}
-    protected function showContent() {echo "Dit is de content en moet overriden worden.";}
+    protected function showContent() 
+    {
+        echo '<nav>' . PHP_EOL;                
+        showNavList ();
+        echo '</nav>' . PHP_EOL;
+    }
+
+    private function showNavList()
+    {
+        echo    '<ul class="menu">' . PHP_EOL;
+        foreach ($data['menu'] as $link => $label)
+        {
+            showNavItem($link, $label);
+        }
+        echo    '</ul>' . PHP_EOL;
+    }
+
     private function showFooter() {echo '<footer><p>&copy; 2023 Nicole Goris</p></footer>';}
 
-    protected function showHeadContent() {
+    protected function showHeadContent() 
+    {
         $this->showTitle();
         $this->showCSSLinks();
     }
  
-    protected function showBodyContent() {
+    protected function showBodyContent() 
+    {
         $this -> showHeader();
         $this -> showMenu();
         $this -> showContent();
--- views/ItemDoc.php
+++ views/ItemDoc.php
@@ -1,22 +1,58 @@
 <?php
 require_once "../views/BasicDoc.php";
 
-abstract class ItemDoc extends BasicDoc {
-    abstract private function getItemFromDb() {}
-    abstract protected function showId() {}
-    abstract protected function showName() {}
-    abstract protected function showImg() {}
-    abstract protected function showPrice() {}
-    abstract protected function showDescription() {}
-    abstract protected function showCartButton() {}      //Alleen als je bent ingelogd
-
-    abstract protected function showContent() {
-        $this->showId(); 
-        $this->showName();
-        $this->showImg(); 
-        $this->showPrice(); 
-        $this->showDescription();
-        $this->showCartButton();                //Alleen als je bent ingelogd
+class ItemDoc extends BasicDoc {
+    private function getItemFromDb() {}
+    protected function showItem($page, $row) 
+    {       
+        $counter = 0;
+        $topNumber = 1;
+        foreach ($item as $row) {
+            $commaPrice = number_format($row['price'], 2, ',', '.');
+            $itemClass = ($counter % 2 == 0) ? 'evenItem' : 'oddItem';
+            echo    '<a class="item" href="index.php?page=details&id=' . $row['id'] . '"><div class="' . $itemClass . '">' . PHP_EOL;
+            switch($page)
+            {
+                case "shop": 
+                    echo '' . $row['id'] . '<br>' . PHP_EOL;
+                    echo '<img src="Images/' . $row['filename'] . '" width="100" height="100" alt="Afbeelding"><br>' . PHP_EOL; 
+                    break;
+                case "top5":
+                    echo '<h2>' . $topNumber . '</h2><br>' . PHP_EOL;
+                    echo '<img src="Images/' . $row['filename'] . '" width="100" height="100" alt="Afbeelding"><br>' . PHP_EOL;
+                    break;
+                case "details":
+                    echo '<img src="Images/' . $row['filename'] . '" width="300" height="300" alt="Afbeelding"><br>' . PHP_EOL;
+                    break;
+            }
+            echo    '<h3>' . $row['name'] . '</h3><br>' . PHP_EOL;
+            switch($page)
+            {
+                case "details":
+                    $row['description'];
+                    break;
+                default:
+                    break;
+            }
+            echo    ' € ' . $commaPrice . 
+                    '<br><br>' . PHP_EOL; 
+            $counter++;
+            $topNumber++;
+        }
+    }
+    protected function showCartButton($page, $row) 
+    {
+        require_once('session_manager.php'); 
+        if (!empty(isUserLoggedIn())) {
+            echo '  <form action="index.php" method="post">
+                        <input type="hidden" name="page" value="' . $page . '">
+                        <input type="hidden" name="id" value="' . $row['id'] . '">
+                        <input type="hidden" name="action" value="storeItemInSession">
+                        <input class="cartButton "type="submit" value="Voeg toe aan winkelwagentje">
+                    </form>';
+        }
+        echo    '</div></a>';
+                $counter++;
     }
 }
 
