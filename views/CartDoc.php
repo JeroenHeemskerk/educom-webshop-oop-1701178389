e3f8ecd98e8571830732223b741b47c6ad51b7c5
Nicole Goris <nicolegoris@hotmail.nl>
2023-12-04 11:01:02 +0100

    #4 winkelwagen werkt

--- views/CartDoc.php
+++ views/CartDoc.php
@@ -1,5 +1,5 @@
 <?php
-require_once "../views/ItemDoc.php";
+require_once "ItemDoc.php";
 
 class CartDoc extends ItemDoc {
     protected function showHeader()
@@ -7,50 +7,52 @@ class CartDoc extends ItemDoc {
         echo 'Winkelwagen';
     }
     protected function showContent()
-    if(isset($_SESSION["cart"])) 
-    {
-        $cart = $_SESSION["cart"];
-        require_once('file_repository.php');
-        $items = getShopItems();
-        $counter = 0;
-        $total = 0;
-        foreach ($cart as $itemId => $quantity) {
-            $row = $items[$itemId];
-            $commaPrice = number_format($row['price'], 2, ',', '.');
-            $subtotal = $row['price'] * $quantity;
-            $commaSubtotal = number_format($subtotal, 2, ',', '.');
-            $shopItemClass = ($counter % 2 == 0) ? 'evenItem' : 'oddItem';
-            echo    '<a class="shopItemCart" href="index.php?page=details&id=' . $row['id'] . '">
-                    <div id="cartItems" class="' .$shopItemClass . '">
-                    <table>
-                    <td id="itemImg"><img src="Images/' . $row['filename'] . '" width="50" height="50" alt="Afbeelding"></td> 
-                    <td id="itemName"><h3>' . $row['name'] . '</h3></td>
-                    <td id="itemQuan">' . $quantity . ' stuk(s)</td>
-                    <td id="price">€ ' . $commaPrice . ' per stuk</td>
-                    <td id="subtotal"><em>subtotaal € ' . $commaSubtotal . '</em></td>
-                    <br><br>
-                    </table>
-                    </div></a>';
-                $total += $subtotal;
-                $counter++;
+    {   
+        if(isset($_SESSION["cart"])) 
+        {
+            $cart = $_SESSION["cart"];
+            require_once('file_repository.php');
+            $items = getShopItems();
+            $counter = 0;
+            $total = 0;
+            foreach ($cart as $itemId => $quantity) {
+                $row = $items[$itemId];
+                $commaPrice = number_format($row['price'], 2, ',', '.');
+                $subtotal = $row['price'] * $quantity;
+                $commaSubtotal = number_format($subtotal, 2, ',', '.');
+                $shopItemClass = ($counter % 2 == 0) ? 'evenItem' : 'oddItem';
+                echo    '<a class="shopItemCart" href="index.php?page=details&id=' . $row['id'] . '">
+                        <div id="cartItems" class="' .$shopItemClass . '">
+                        <table>
+                        <td id="itemImg"><img src="Images/' . $row['filename'] . '" width="50" height="50" alt="Afbeelding"></td> 
+                        <td id="itemName"><h3>' . $row['name'] . '</h3></td>
+                        <td id="itemQuan">' . $quantity . ' stuk(s)</td>
+                        <td id="price">€ ' . $commaPrice . ' per stuk</td>
+                        <td id="subtotal"><em>subtotaal € ' . $commaSubtotal . '</em></td>
+                        <br><br>
+                        </table>
+                        </div></a>';
+                    $total += $subtotal;
+                    $counter++;
+            }
+            $commaTotal = number_format($total, 2, ',', '.');
+            echo    '<div class="total">
+                        <p>
+                            <h3>Totaal: € ' . $commaTotal . '</h3>
+                            <form action="index.php" method="post">
+                            <input type="hidden" name="page" value="succeed">
+                            <input type="hidden" name="action" value="insertOrderInDb">
+                            <input class="cartButton "type="submit" value="Afrekenen">
+                            </form>
+                        </p>                
+                    </div>';
+        } else {
+            echo    '<div>
+                        <p>
+                            <h3>Uw winkelwagen is leeg. Kijk snel in de spellenwinkel!</h3>
+                        </p>
+                    </div>';
         }
-        $commaTotal = number_format($total, 2, ',', '.');
-        echo    '<div class="total">
-                    <p>
-                        <h3>Totaal: € ' . $commaTotal . '</h3>
-                        <form action="index.php" method="post">
-                        <input type="hidden" name="page" value="succeed">
-                        <input type="hidden" name="action" value="insertOrderInDb">
-                        <input class="cartButton "type="submit" value="Afrekenen">
-                        </form>
-                    </p>                
-                </div>';
-    } else {
-        echo    '<div>
-                    <p>
-                        <h3>Uw winkelwagen is leeg. Kijk snel in de spellenwinkel!</h3>
-                    </p>
-                </div>';
     }
 }
 
